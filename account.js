4c8dca2b084e65acea763f121b3c1f277bf1c782
Ghasem Kiani <ghasemkiani@gmail.com>
2025-08-30 23:04:59 +0330

    update

--- account.js
+++ account.js
@@ -28,6 +28,42 @@ class Account extends cutil.mixin(Obj, iwclient) {
     const { address } = account;
     return await client.toGetBalance(address);
   }
+  async toGetTokenAddressBalance$(tokenAddress) {
+    const account = this;
+    const { client } = account;
+    const { address } = account;
+    return await client.toGetTokenAddressBalance$(address, tokenAddress);
+  }
+  async toGetTokenAddressBalance_(tokenAddress) {
+    const account = this;
+    const { client } = account;
+    const { address } = account;
+    return await client.toGetTokenAddressBalance_(address, tokenAddress);
+  }
+  async toGetTokenAddressBalance(tokenAddress) {
+    const account = this;
+    const { client } = account;
+    const { address } = account;
+    return await client.toGetTokenAddressBalance(address, tokenAddress);
+  }
+  async toGetTokenBalance$(tokenId) {
+    const account = this;
+    const { client } = account;
+    const { address } = account;
+    return await client.toGetTokenBalance$(address, tokenId);
+  }
+  async toGetTokenBalance_(tokenId) {
+    const account = this;
+    const { client } = account;
+    const { address } = account;
+    return await client.toGetTokenBalance_(address, tokenId);
+  }
+  async toGetTokenBalance(tokenId) {
+    const account = this;
+    const { client } = account;
+    const { address } = account;
+    return await client.toGetTokenBalance(address, tokenId);
+  }
 }
 
 export { Account };
